a9799687812c8cb42211600fac8c539ee09cef7b
BicycleBi-Jeanre <jeanre@bicyclebi.com>
2024-06-15 11:53:46 +0200

    Budget And Account Balances working final

---
+++ Scripts/cleaning-connections.sql
@@ -0,0 +1,25 @@
+select
+	pg_terminate_backend(pg_stat_activity.pid)
+--	*
+from 
+	pg_stat_activity
+where 
+	pg_stat_activity.datname = 'JPersonal'
+	
+and
+	pg_stat_activity.application_name not like 'DBeaver%'
+
+
+
+  and 
+ 	pg_stat_activity.state = 'active';
+ 	
+ 
+ 
+select
+--	pg_terminate_backend(pg_stat_activity.pid)
+	*
+from 
+	pg_stat_activity
+where 
+	pg_stat_activity.datname = 'JPersonal'
