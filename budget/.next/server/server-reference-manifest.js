69b3fc8fe6a2cb4a3e596fa31773c029ee0f4e02
BicycleBi-Jeanre <jeanre@bicyclebi.com>
2024-12-15 10:47:27 +0200

    "adding postgres connection"

---
+++ budget/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gee3Xpxuv4AR2xRIk8rRli88wmKtpkRpYwtX5TwERew=\"\n}"
---
+++ budget/src/app/page.tsx
@@ -0,0 +1,8 @@
+// Rolling back changes
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Budget App</h1>
+    </main>
+  );
+}
