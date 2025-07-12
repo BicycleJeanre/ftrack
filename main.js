bd2bb9d19fb69a98a768384699f5ab3583f93b04
BicycleBi-Jeanre <jeanre@bicyclebi.com>
2025-07-12 11:00:23 +0200

    Setup: Basic Electron app initialization
    Changed files:
    - package.json: set Electron entry point and start script
    - main.js: created Electron main process file
    - preload.js: created preload script for context isolation

---
+++ main.js
@@ -0,0 +1,30 @@
+// main.js
+// Main process for Electron app
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+  win.loadFile('pages/home.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
